ccb84753c61cd438ec83cf7839b9d734f67bb743
Steffan Sluis <steffansluis@gmail.com>
2016-10-17 03:11:45 +0200

    Add content-type

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const express = require('express');
 const SVG = require('./components/svg');
 const Card = require('./components/card');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const XML_TAG = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n";
 
 const server = express();
 
 server.get('/', (req, res) => {
+  res.set({
+    'Content-Type': 'image/svg+xml',
+    'Content-Disposition': 'filename="card.svg"'
+  });
+  
   res.send(XML_TAG + ReactDOMServer.renderToString(<SVG><Card {...req.query} /></SVG>));
 });
 
